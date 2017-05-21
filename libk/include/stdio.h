bb59043bf305dba53e294f081a693ac8d0f7a318
no92 <no92.mail@gmail.com>
2017-05-21 21:18:43 +0200

    add gcc format checking to v(sn)printf(3)

--- libk/include/stdio.h
+++ libk/include/stdio.h
@@ -9,5 +9,5 @@ __attribute__((format(printf, 1, 2))) int printf(const char * restrict format, .
 
 __attribute__((format(printf, 1, 4))) int snprintf(char * restrict s, size_t n, const char *restrict format, ...);
 
-int vprintf(const char * restrict format, va_list arg);
-int vsnprintf(char * restrict s, size_t n, const char * restrict format, va_list arg);
+__attribute__((format(printf, 1, 0))) int vprintf(const char * restrict format, va_list arg);
+__attribute__((format(printf, 1, 0))) int vsnprintf(char * restrict s, size_t n, const char * restrict format, va_list arg);
