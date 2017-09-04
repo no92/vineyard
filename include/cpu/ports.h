f8f752f621db73262521dd89ca19b9b083f4ff01
no92 <no92.mail@gmail.com>
2017-09-04 16:27:05 +0200

    Add wrapper functions for inl and outl

--- include/cpu/ports.h
+++ include/cpu/ports.h
@@ -3,5 +3,7 @@
 #include <stdint.h>
 
 uint8_t inb(uint16_t port);
+uint32_t inl(uint16_t port);
 
 void outb(uint16_t port, uint8_t val);
+void outl(uint16_t port, uint32_t val);
--- kernel/cpu/ports.c
+++ kernel/cpu/ports.c
@@ -10,6 +10,18 @@ inline uint8_t inb(uint16_t port) {
 	return ret;
 }
 
+inline uint32_t inl(uint16_t port) {
+	uint32_t ret;
+
+	asm volatile ("inl %1, %0" : "=a" (ret) : "Nd" (port));
+
+	return ret;
+}
+
 inline void outb(uint16_t port, uint8_t val) {
 	asm volatile ("outb %0, %1" : : "a" (val), "Nd" (port));
+}
+
+inline void outl(uint16_t port, uint32_t val) {
+	asm volatile ("outl %0, %1" : : "a" (val), "Nd" (port));
 }
