e119d247ce34e0fbd18ddd5d610b3c50ccada961
no92 <no92.mail@gmail.com>
2018-03-18 21:35:11 +0100

    Fix an assembler warning about intermediate values in boot code
    Before this, YASM kept throwing the warning 'value does not fit in
    signed 32 bit field'. Negating the value solves the problem.
    Additionally, constants were introduced to clarify the purpose of the
    values in question instead of them being magic numbers.

--- kernel/init/boot.s
+++ kernel/init/boot.s
@@ -7,6 +7,12 @@ MB2_MAGIC equ 0xE85250D6
 MB2_TAG_TERMINATE equ 0
 MB2_TAG_FRAMEBUFFER equ 5
 
+; this is the offset we add to the kernel's physical address (1 MiB)
+; its value is 3 GiB (= 0xC0000000), but in order to prevent warnings we use the corresponding signed value
+KERNEL_VIRTUAL_OFFSET equ -0x40000000
+; for convenience, we define KERNEL_VIRTUAL_UOFFSET as the true (unsigned value), mostly for bitshifting
+KERNEL_VIRTUAL_UOFFSET equ 0xC0000000
+
 ; we put the header in its own section to make sure it is located in the first 32 KiB of the binary
 [section .multiboot2 align=8]
 mb2_header_start:
@@ -39,7 +45,7 @@ mb2_header_end:
 
 [global _start]
 _start:
-	mov ecx, (boot_pagedir - 0xC0000000)
+	mov ecx, dword (boot_pagedir - KERNEL_VIRTUAL_OFFSET)
 	mov cr3, ecx
 	mov ecx, cr0
 	or ecx, 0x80000000
@@ -59,7 +65,7 @@ _start:
 	mov ebp, stack_top
 
 	; set up the arguments for the kernel
-	add ebx, 0xC0000000
+	add ebx, KERNEL_VIRTUAL_OFFSET
 	push ebx ; holds the physical address of the multiboot struct
 	push eax ; holds the magic number
 
@@ -82,13 +88,13 @@ stack_top:
 [section .data]
 [align 0x1000]
 boot_pagedir:
-	dd (boot_pagetab_low - 0xC0000000) + 0x03
-	dd (boot_pagetab_high - 0xC0000000) + 0x03
-	times ((0xC0000000 >> 22) - 2) dd 0x00
-	dd (boot_pagetab_low - 0xC0000000) + 0x03
-	dd (boot_pagetab_high - 0xC0000000) + 0x03
-	times (1021 - (0xC0000000 >> 22)) dd 0x00
-	dd (boot_pagedir - 0xC0000000) + 0x03
+	dd (boot_pagetab_low - KERNEL_VIRTUAL_OFFSET) + 0x03
+	dd (boot_pagetab_high - KERNEL_VIRTUAL_OFFSET) + 0x03
+	times ((KERNEL_VIRTUAL_UOFFSET >> 22) - 2) dd 0x00
+	dd (boot_pagetab_low - KERNEL_VIRTUAL_OFFSET) + 0x03
+	dd (boot_pagetab_high - KERNEL_VIRTUAL_OFFSET) + 0x03
+	times (1021 - (KERNEL_VIRTUAL_UOFFSET >> 22)) dd 0x00
+	dd (boot_pagedir - KERNEL_VIRTUAL_OFFSET) + 0x03
 boot_pagetab_low:
 	%assign i 0
 	%rep 1024
