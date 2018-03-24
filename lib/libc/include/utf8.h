8728b6052db01572b144a561ed57ecc7b542fbb9
no92 <no92.mail@gmail.com>
2018-03-24 12:10:22 +0100

    Remove unnecessary library functions

--- lib/libc/include/utf8.h
+++ lib/libc/include/utf8.h
@@ -5,5 +5,3 @@
 #include <stdint.h>
 
 bool u8cont(const char b);
-__attribute__((pure, nonnull(1))) size_t u8len(const char *c);
-__attribute__((pure, nonnull(1))) size_t u8strlen(const char *str);
